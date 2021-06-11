190bf32e842cab54a690de8e659379530c0b936c
ezidol <goodyoungmin@naver.com>
2021-06-11 17:31:27 +0900

    Make method - Withdraw

--- main.go
+++ main.go
@@ -10,4 +10,9 @@ func main() {
 	account := accounts.NewAccount("Peter")
 	account.Deposit(100)
 	fmt.Println(account.Balance())
+	var err = account.Withdraw((150))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
